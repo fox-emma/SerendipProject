d5bf0a44fad9b1495833319cc62c4726e2b6171e
Emma Fox <ejfox824@gmail.com>
2020-05-25 09:56:04 -0400

    First commit

---
+++ Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+class Main extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Main;
